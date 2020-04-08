10a139233a2203a608e11c7e3fe84d79dda3f55e
robertbates1995 <robertbates1995@users.noreply.github.com>
2020-04-08 12:21:13 -0700

    added drawers

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,26 +8,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: _buildAppBar(),
-        body: _buildContainer());
+    return Scaffold(appBar: _buildAppBar(), body: _buildContainer(), drawer: _buildDrawer(),);
+  }
+
+  Drawer _buildDrawer() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          Text('test')
+        ],
+      ),
+    );
   }
 
   Container _buildContainer() {
     return Container(
-          constraints: BoxConstraints.expand(),
-          child: Image.asset(
-            'assets/images/Valorant Logo.png',
-          ));
+        constraints: BoxConstraints.expand(),
+        color: Color(0xff0f1922),
+        child: Image.asset(
+          'assets/images/Valorant Logo.png',
+        ));
   }
 
   AppBar _buildAppBar() {
     return AppBar(
-        centerTitle: true,
-        title: Text(
-          'VALORANT',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        ),
-      );
+      centerTitle: true,
+      title: _mainTitleMaker(),
+    );
+  }
+
+  //made following function to test out how extract=>method works
+  Text _mainTitleMaker() {
+    return Text(
+      'VALORANT',
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color(0xffff4656)),
+    );
   }
 }
